5f807516b6133386bec9709320c74b2d66203160
alangumer <alangumer@gmail.com>
2016-02-28 10:45:16 -0600

    added providers module

--- src/controllers.php
+++ src/controllers.php
@@ -155,6 +155,29 @@ $app->put('/banks/edit', function () use ($app) {
 /* end banks */
 
 
+/* start providers */
+$app->get('/providers', function () use ($app) {
+  return $app->json( $app['db']->fetchAll( "SELECT * FROM Provider order by id desc", array() ) );
+});
+
+$app->get('/providers/{id}', function ($id) use ($app) {
+  return $app->json( $app['db']->fetchAssoc( "SELECT * FROM Provider where id = ?", array($id) ) );
+});
+
+$app->post('/providers/add', function () use ($app) {
+  $data = json_decode( file_get_contents("php://input"), true );
+
+  $app['db']->insert('Provider', $data);
+  return $app->json( formatResponse( $app['db']->lastInsertId() ) );
+});
+
+$app->put('/providers/edit', function () use ($app) {
+  $data = json_decode( file_get_contents("php://input"), true );
+
+  $app['db']->update( 'Provider', $data, array( 'id' => $data['id'] ) );
+  return $app->json( formatResponse( $data['id'] ) );
+});
+/* end providers */
 
 
 
