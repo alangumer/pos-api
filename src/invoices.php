51cf039034fd7256415ae8ea0f4e256803161925
alangumer <alangumer@gmail.com>
2016-04-19 12:37:14 -0600

    Fixed credit

--- src/invoices.php
+++ src/invoices.php
@@ -33,22 +33,24 @@ $invoices->post('/add', function () use ($app) {
     from Invoice i
     join Allocation a
       on a.invoice_id = i.id and i.customer_id = " . $data['customerId'] . "
-    group by i.ref
-  having SUM(a.amount) > 0";
+    group by i.ref";
+  // having SUM(a.amount) > 0";
   
-  $owinCustomer = $app['db']->fetchAssoc( $sql, array() );
+  $owingCustomer = $app['db']->fetchAssoc( $sql, array() );
   
   // get credit_limit customer
-  $creditLimit = $app['db']->fetchAssoc( "select ct.credit_limit from Customer c join CustomerType ct on c.customer_type_id = ct.id where c.id = 17", array() );
+  $creditLimit = $app['db']->fetchAssoc( "select ct.credit_limit from Customer c join CustomerType ct on c.customer_type_id = ct.id where c.id = " . $data['customerId'], array() );
   
   // insert invoice details
   $items = $data['items'];
   $grandTotal = 0;
   for( $i = 0; $i < sizeof($items); $i++ ) {
-    $grandTotal += (double) $items[$i]['total'];
+    $grandTotal += ( double ) $items[$i]['total'];
   }
+
+  $credit = $grandTotal - $data['payment'];
   
-  if ( ( $owinCustomer + $grandTotal ) <= $creditLimit['credit_limit'] ) {
+  if ( ( $credit + $owingCustomer['Owing'] ) <= $creditLimit['credit_limit'] ) {
     // insert invoice
     $app['db']->insert('Invoice', array(
       'customer_id' => $data['customerId']
@@ -73,24 +75,27 @@ $invoices->post('/add', function () use ($app) {
       'amount' => $grandTotal,
       'type' => 'receivable'
     ));
+
+    if ( $data['payment'] > 0 ) {
     
-    // insert payment
-    $app['db']->insert('Payment', array(
-      'amount' => $data['payment'],
-    ));
-    $paymentId = $app['db']->lastInsertId();
-    
-    // insert allocation - payment
-    $app['db']->insert('Allocation', array(
-      'payment_id' => $paymentId,
-      'invoice_id' => $invoiceId,
-      'amount' => -$data['payment'],
-      'type' => 'paid'
-    ));
+      // insert payment
+      $app['db']->insert('Payment', array(
+        'amount' => $data['payment'],
+      ));
+      $paymentId = $app['db']->lastInsertId();
+      
+      // insert allocation - payment
+      $app['db']->insert('Allocation', array(
+        'payment_id' => $paymentId,
+        'invoice_id' => $invoiceId,
+        'amount' => -$data['payment'],
+        'type' => 'paid'
+      ));
+    }
     
     return $app->json( formatResponse( $invoiceId ) );
   } else {
-    return $app->json( array( 'error' => 'El cliente ha excedio su limite de credito' ) );
+    return $app->json( array( 'error' => 'No se puede ingresar la factura porque el cliente debe ' . $owingCustomer['Owing'] . ' y con este monto deberia ' . ( $credit + $owingCustomer['Owing'] ) . ' que excede el limite de credito de ' . $creditLimit['credit_limit'], 'sql' => $sql ) );
   }
 });
 
